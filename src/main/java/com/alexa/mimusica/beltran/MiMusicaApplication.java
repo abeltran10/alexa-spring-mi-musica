525290ed3966202d5430839174db86708fa592a5
abeltran <almarti@uji.es>
2020-06-07 22:35:41 +0200

    Primer commit

---
+++ src/main/java/com/alexa/mimusica/beltran/MiMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.alexa.mimusica.beltran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMusicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiMusicaApplication.class, args);
+    }
+
+}
